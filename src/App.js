a11f277f7d2bb0c09d509f71cab375707dea80d2
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2024-10-20 20:27:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './Home';
+import Dashboard from './components/Dashboard';
+import LoginModal from './components/LoginModal'; // Pastikan ini sudah benar
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen"> {/* Menambahkan flex container */}
+        <Navbar />
+        <main className="flex-grow"> {/* flex-grow untuk mengisi ruang yang tersedia */}
+          <Routes>
+            {/* Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginModal />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
