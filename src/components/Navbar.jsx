92289c4c5759cd76043643957cceeef9536a9007
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-10-22 16:39:49 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import LoginModal from './LoginModal'; // Ensure path is correct
 import CartSummary from './CartSummary'; // Import CartSummary component
+import HistorySummary from './HistorySummary'; // Import HistorySummary component
 import { FaShoppingCart } from 'react-icons/fa'; // Import cart icon
+import { FaHistory } from 'react-icons/fa'; // Import history icon
 import { supabase } from '../supabaseClient'; // Ensure this path is correct
 
 const Navbar = () => {
     const [isModalOpen, setModalOpen] = useState(false); // State for modal
     const [isCartOpen, setCartOpen] = useState(false); // State for cart visibility
+    const [isHistoryOpen, setHistoryOpen] = useState(false); // State for history visibility
     const [cartItemCount, setCartItemCount] = useState(0); // State for cart item count
+    const [itemCount, setItemCount] = useState(0); // State for total item count
     const [user, setUser] = useState(null); // State to store user information
 
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
     const toggleCart = () => setCartOpen(!isCartOpen); // Function to toggle cart visibility
     const closeCart = () => setCartOpen(false); // Function to close cart
+    const toggleHistory = () => setHistoryOpen(!isHistoryOpen); // Function to toggle cart visibility    
+    const closeHistory = () => setHistoryOpen(false); // Function to close cart
+
 
     // Function for Logout
     const handleLogout = async () => {
@@ -34,18 +41,33 @@ const Navbar = () => {
             setUser(currentUser);
 
             if (currentUser) {
-                // Fetch cart items for the logged-in user
-                const { data, error } = await supabase
+                // Fetch cart items for the logged-in user with status 'Add to Cart'
+                const { data: cartData, error: cartError } = await supabase
+                    .from('user_cart_summary') // Tabel yang menyimpan data keranjang
+                    .select('quantity') // Ambil kolom quantity
+                    .eq('user_id', currentUser.id) // Filter berdasarkan user_id
+                    .eq('status', 'Add to Cart');
+
+                if (cartError) {
+                    console.error('Error fetching cart items:', cartError.message);
+                } else {
+                    // Hitung total item dalam keranjang dengan status 'Add to Cart'
+                    const totalCartItems = cartData.reduce((acc, item) => acc + item.quantity, 0);
+                    setCartItemCount(totalCartItems); // Update state cartItemCount
+                }
+
+                // Fetch all cart items for the logged-in user regardless of status
+                const { data: allData, error: allError } = await supabase
                     .from('user_cart_summary') // Tabel yang menyimpan data keranjang
                     .select('quantity') // Ambil kolom quantity
                     .eq('user_id', currentUser.id); // Filter berdasarkan user_id
 
-                if (error) {
-                    console.error('Error fetching cart items:', error.message);
+                if (allError) {
+                    console.error('Error fetching all cart items:', allError.message);
                 } else {
-                    // Hitung total item dalam keranjang
-                    const totalItems = data.reduce((acc, item) => acc + item.quantity, 0);
-                    setCartItemCount(totalItems); // Update state cartItemCount
+                    // Hitung total item dalam keranjang tanpa memperhatikan status
+                    const totalItems = allData.reduce((acc, item) => acc + item.quantity, 0);
+                    setItemCount(totalItems); // Update state itemCount
                 }
             }
         };
@@ -71,11 +93,24 @@ const Navbar = () => {
                         const totalItems = data.reduce((acc, item) => acc + item.quantity, 0);
                         setCartItemCount(totalItems);
                     }
-                };
-                fetchCartData();
-            } else {
-                setCartItemCount(0); // Reset cart count if user logs out
-            }
+                    // Fetch all cart items for the logged-in user regardless of status
+                    const { data: allData, error: allError } = await supabase
+                        .from('user_cart_summary') // Tabel yang menyimpan data keranjang
+                        .select('quantity') // Ambil kolom quantity
+                        .eq('user_id', currentUser.id); // Filter berdasarkan user_id
+
+                    if (allError) {
+                        console.error('Error fetching all cart items:', allError.message);
+                    } else {
+                        // Hitung total item dalam keranjang tanpa memperhatikan status
+                        const totalItems = allData.reduce((acc, item) => acc + item.quantity, 0);
+                        setItemCount(totalItems); // Update state itemCount
+                    }
+                        };
+                        fetchCartData();
+                    } else {
+                        setCartItemCount(0); // Reset cart count if user logs out
+                    }
         });
 
         return () => {
@@ -140,13 +175,25 @@ const Navbar = () => {
                             </button>
                         </div>
 
+                         {/* Histori (History Icon) */}
+                         <div className="relative">
+                            <button className={`text-white relative ${itemCount > 0 ? 'text-yellow-400' : ''}`} onClick={toggleHistory}>
+                                <FaHistory size={24} />
+                                {itemCount > 0 && (
+                                    <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                                        {itemCount}
+                                    </span>
+                                )}
+                            </button>
+                        </div>
+
                         {/* Login/Logout Section */}
                         {user ? (
                             <div className="flex items-center space-x-4">
                                 <span className="text-white font-bold">{user.email}</span>
                                 <button
                                     onClick={handleLogout}
-                                    className="bg-transparent border border-white text-white text-base font-bold py-2 px-5 rounded-none hover:bg-white hover:text-red-500 transition duration-300"
+                                    className="bg-transparent border border-white text-white text-base font-bold py-2 px-5 rounded hover:bg-white hover:text-red-500 transition duration-300"
                                 >
                                     Logout
                                 </button>
@@ -169,6 +216,9 @@ const Navbar = () => {
 
             {/* Cart Summary Modal */}
             {isCartOpen && <CartSummary onClose={() => setCartOpen(false)} />} {/* Menutup modal */}
+
+            {/* History Summary Modal */}
+            {isHistoryOpen && <HistorySummary onClose={() => setHistoryOpen(false)} />} {/* Menutup modal */}
         </React.Fragment>
     );
 };
